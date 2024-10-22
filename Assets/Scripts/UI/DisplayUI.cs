b31119d40107d4ed0b431ad1e4d9cae0d731b32e
George Jimy <knivespeans@gmail.com>
2024-10-22 16:35:27 -0500

    Display UI Trigger

--- Assets/Scripts/UI/DisplayUI.cs
+++ Assets/Scripts/UI/DisplayUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisplayDropdown : MonoBehaviour
+public class DisplayUI : MonoBehaviour
 {
     public GameObject dropDown;
 
